64b6d6570a9bb8d6369dbcc4fd96dff7e3335138
mike <michael.mcclosky@gmail.com>
2023-11-06 17:22:35 -0500

    Automated commit from Start-DailySync@20231106T172234

--- Documents/PowerShell/Profile.ps1
+++ Documents/PowerShell/Profile.ps1
@@ -11,6 +11,21 @@ function Open-DotfilesWorkspace {
 }
 Set-Alias "dfws" "Open-DotfilesWorkspace"
 
+function Sync-Dotfiles {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $commitMessage
+    )
+    Write-Host "Syncing dotfiles to prevent conficts." -ForegroundColor Blue
+    Use-DotfilesRepo pull
+    Write-Host "Adding all tracked files under current location to staging." -ForegroundColor Blue
+    Use-DotfilesRepo add . -u
+    Write-Host "Commiting and pushing to remote." -ForegroundColor Blue
+    Use-DotfilesRepo commit -m $commitMessage
+    Use-DotfilesRepo push
+    Write-Host "Done." -ForegroundColor Blue
+}
+
 # First time setup Notes:
 #   VSCode registry stuff after scoop install
 #   Winterm registry stuff after scoop install
@@ -25,20 +40,22 @@ function Start-DailySync {
         [switch] $force
     )
 
-    $lastRunDate = (scoop config).last_update.Date
+    $lastRunTime = (scoop config).last_update
+    $lastRunDate = $lastRunTime.Date
     $todayDate = (Get-Date).Date
-    if (-not $force -and ($lastRunDate -ge $todayDate)) { return }
+
+    if (-not $force) {
+        if ($lastRunDate -ge $todayDate) {
+            Write-Host "Last sync time: $lastRunTime. Will not force sync. Use 'Start-DailySync -Force' to force sync." -ForegroundColor Yellow
+            return;
+        }
+    }
 
     scoop update
-    Write-Host "Syncing dotfiles to prevent conficts." -ForegroundColor Blue
-    Use-DotfilesRepo pull
-    Write-Host "Adding all tracked files under current location to staging." -ForegroundColor Blue
-    Use-DotfilesRepo add . -u
-    Write-Host "Commiting and pushing to remote." -ForegroundColor Blue
+    scoop export | Out-File "$configRoot\scoop\export.json" -Encoding ascii
     $dateString = Get-Date -Format 'yyyyMMddTHHmmss'
-    Use-DotfilesRepo commit -m "Automated commit from Start-DailySync@$dateString"
-    Use-DotfilesRepo push
-    Write-Host "Done." -ForegroundColor Blue
+    $commitMsg = "Automated commit from Start-DailySync@$dateString"
+    Sync-Dotfiles $commitMsg
 }
 
 function Start-Shell {
