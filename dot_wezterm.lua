66d44da5acb1f11c0bc5d60e2a2d472b46c6f2a7
grumbull <michael.mcclosky@gmail.com>
2024-03-30 20:02:40 -0400

    recent updates

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,10 +5,9 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.color_scheme = 'catppuccin-mocha'
+config.color_scheme = 'Kanagawa (Gogh)'
 config.font = wezterm.font 'CaskaydiaCove Nerd Font Mono'
 config.font_size = 14.0
-
 
 -- and finally, return the configuration to wezterm
 return config
