d594e77fb078b1cc913f40bb581a3faf1d8ec03f
grumbull <michael.mcclosky@gmail.com>
2024-04-29 17:19:29 -0400

    wezterm - remove opacity

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,28 +1,36 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
-
--- This will hold the configuration.
+local wezterm = require("wezterm")
+local mux = wezterm.mux
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-config.color_scheme = 'Kanagawa (Gogh)'
+-- Settings
+config.color_scheme = "Kanagawa (Gogh)"
 config.font = wezterm.font_with_fallback({
-  {family = 'CaskaydiaCove Nerd Font Mono', scale = 1.0 },
-  {family = 'Cascadia Code', scale = 1.0 },
-  {family = 'Consolas', scale = 1.0 },
+	{ family = "CaskaydiaCove Nerd Font Mono", scale = 1.0 },
+	{ family = "Cascadia Code", scale = 1.0 },
+	{ family = "Consolas", scale = 1.0 },
 })
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
+config.window_background_opacity = 0.95
 config.scrollback_lines = 3000
 
--- TODO #mac install location is different on MacOS
-config.default_prog = { 'C:\\Users\\mike\\scoop\\shims\\nu.EXE' }
-
-wezterm.on('gui-startup', function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
+-- Window startup location
+-- TODO #mac, window position might be bad.
+local taskbar_height = 75
+local background_padding = 100
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	local main_screen = wezterm.gui.screens().main
+	window:gui_window():set_position(background_padding, background_padding)
+	window:gui_window():set_inner_size(
+		main_screen.width - background_padding * 2,
+		main_screen.height - background_padding * 2 - taskbar_height
+	)
 end)
 
+-- TODO #mac install location is different on MacOS
+config.default_prog = { "C:\\Users\\mike\\scoop\\shims\\nu.EXE" }
 
 -- and finally, return the configuration to wezterm
 return config
