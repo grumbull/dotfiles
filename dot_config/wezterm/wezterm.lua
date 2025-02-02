bad3bd4cf579d352ee61f6e7af4edb7841b7174c
grumbull <michael.mcclosky@gmail.com>
2025-02-02 17:53:12 -0500

    Tab management in wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 local mux = wezterm.mux
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 -- Settings
 config.color_scheme = "Kanagawa (Gogh)"
@@ -13,18 +13,55 @@ config.font = wezterm.font_with_fallback({
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+-- TODO #mac install location is different on MacOS
+config.default_prog = { "C:\\Users\\mike\\scoop\\shims\\nu.EXE" }
 
--- Window startup location
--- TODO #mac, window position might be bad.
-local taskbar_height = 75
-local background_padding = 100
+config.keys = {}
+
+-- Tab management
+local tab_mod <const> = 'ALT'
+-- Create a new tab.
+table.insert(config.keys, {
+  key = '=',
+  mods = tab_mod,
+  action = act.SpawnTab 'CurrentPaneDomain',
+})
+-- Delete the current tab.
+table.insert(config.keys, {
+  key = '-',
+  mods = tab_mod,
+  action = act.CloseCurrentTab { confirm = false }, 
+})
+-- Select tab by number.
+for i = 1, 9 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = tab_mod,
+    action = act.ActivateTab(i - 1),
+  })
+end
+table.insert(config.keys, {
+  key = '0',
+  mods = tab_mod,
+  action = act.ActivateTab(9),
+})
+-- Go to last tab.
+table.insert(config.keys, {
+  key = 'l',
+  mods = tab_mod,
+  action = act.ActivateLastTab, 
+})
+
 wezterm.on("gui-startup", function()
 	local tab, pane, window = mux.spawn_window{}
 	window:gui_window():maximize()
 end)
 
--- TODO #mac install location is different on MacOS
-config.default_prog = { "C:\\Users\\mike\\scoop\\shims\\nu.EXE" }
+wezterm.on('update-right-status', function(window, pane)
+  window:set_right_status(window:active_workspace())
+end)
 
 -- and finally, return the configuration to wezterm
 return config
