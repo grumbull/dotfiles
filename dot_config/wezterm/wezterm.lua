a812b1a1655112b37e672e269f60bc21dfed779a
grumbull <michael.mcclosky@gmail.com>
2025-02-02 18:26:40 -0500

    Pane management for wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -18,7 +18,13 @@ config.use_fancy_tab_bar = false
 -- TODO #mac install location is different on MacOS
 config.default_prog = { "C:\\Users\\mike\\scoop\\shims\\nu.EXE" }
 
+config.disable_default_key_bindings = true
 config.keys = {}
+table.insert(config.keys, {
+  key = 'P',
+  mods = 'CTRL|SHIFT',
+  action = act.ActivateCommandPalette,
+})
 
 -- Tab management
 local tab_mod <const> = 'ALT'
@@ -30,7 +36,7 @@ table.insert(config.keys, {
 })
 -- Delete the current tab.
 table.insert(config.keys, {
-  key = '-',
+  key = 'q',
   mods = tab_mod,
   action = act.CloseCurrentTab { confirm = false }, 
 })
@@ -49,9 +55,78 @@ table.insert(config.keys, {
 })
 -- Go to last tab.
 table.insert(config.keys, {
-  key = 'l',
+  key = '-',
   mods = tab_mod,
   action = act.ActivateLastTab, 
+})
+
+-- Pane Management
+local pane_mod <const> = 'CTRL|SHIFT'
+-- Split into left/right
+table.insert(config.keys, {
+  key = '+',
+  mods = pane_mod,
+  action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }, 
+})
+-- Split into top/bottom
+table.insert(config.keys, {
+  key = '_',
+  mods = pane_mod,
+  action = act.SplitVertical { domain = 'CurrentPaneDomain' }, 
+})
+-- Close current pane 
+table.insert(config.keys, {
+  key = 'q',
+  mods = pane_mod,
+  action = act.CloseCurrentPane { confirm = false }, 
+})
+-- Activate pane left
+table.insert(config.keys, {
+  key = 'h',
+  mods = pane_mod,
+  action = act.ActivatePaneDirection 'Left', 
+})
+-- Activate pane right
+table.insert(config.keys, {
+  key = 'l',
+  mods = pane_mod,
+  action = act.ActivatePaneDirection 'Right', 
+})
+-- Activate pane up
+table.insert(config.keys, {
+  key = 'k',
+  mods = pane_mod,
+  action = act.ActivatePaneDirection 'Up', 
+})
+-- Activate pane down
+table.insert(config.keys, {
+  key = 'j',
+  mods = pane_mod,
+  action = act.ActivatePaneDirection 'Down', 
+})
+-- Adjust pane size left
+table.insert(config.keys, {
+  key = 'LeftArrow',
+  mods = pane_mod,
+  action = act.AdjustPaneSize {'Left', 5 } 
+})
+-- Adjust pane size right
+table.insert(config.keys, {
+  key = 'RightArrow',
+  mods = pane_mod,
+  action = act.AdjustPaneSize {'Right', 5 } 
+})
+-- Adjust pane size up
+table.insert(config.keys, {
+  key = 'UpArrow',
+  mods = pane_mod,
+  action = act.AdjustPaneSize {'Up', 5 } 
+})
+-- Adjust pane size down
+table.insert(config.keys, {
+  key = 'DownArrow',
+  mods = pane_mod,
+  action = act.AdjustPaneSize {'Down', 5 } 
 })
 
 wezterm.on("gui-startup", function()
